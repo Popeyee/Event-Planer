9850a5b8902c0179869abbc98d048850dbdb4126
Popeyee <elhachimiali1@gmail.com>
2025-11-19 11:17:58 -0500

    fix- small fixes

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,14 +1,4 @@
-import BookEvent from "@/components/BookEvent";
-import AgendaItem from "@/components/EventAgenda";
-import EventCard from "@/components/EventCard";
-import EventDetailItem from "@/components/EventDetailItem";
-import EventDetails from "@/components/EventDEtails";
-import EventTags from "@/components/EventTags";
-import { EventAttrs } from "@/database";
-import { getSimilarEventBySlug } from "@/lib/actions/event.actions";
-import { cacheLife } from "next/cache";
-import Image from "next/image";
-import { notFound } from "next/navigation";
+import EventDetails from "@/components/EventDetails";
 import { Suspense } from "react";
 
 // Shape of the event returned from the /api/events/[slug] endpoint.
